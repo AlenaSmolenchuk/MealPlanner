2e94eda0c3557ccf94cb5d544161b4467c475375
Alena <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-04-12 19:47:39 +0300

    Create Meal.java

---
+++ src/mealplanner/Meal.java
@@ -0,0 +1,32 @@
+* this class will be needed later *
+
+package mealplanner;
+
+public class Meal {
+
+    private String name;
+    private String[] ingredients;
+
+    public Meal(String name, String[] ingredients) {
+
+        this.name = name;
+        this.ingredients = ingredients;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String[] getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String[] ingredients) {
+        this.ingredients = ingredients;
+    }
+}

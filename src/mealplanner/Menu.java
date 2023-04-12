655700e20daac70ec35ab7edda8637624f6d56ce
Alena <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-04-12 21:06:30 +0300

    Create Menu.java

---
+++ src/mealplanner/Menu.java
@@ -0,0 +1,29 @@
+package mealplanner;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Menu {
+    private HashMap<String,Meal> menu;
+    Scanner sc = new Scanner(System.in);
+    Recept recept = new Recept();
+
+    public void start(){
+        System.out.println("What would you like to do (add, show, exit)?");
+        String output = sc.nextLine();
+        switch (output){
+            case "add":
+                menu = recept.addRecept();
+                start();
+            case "show":
+                recept.printRecept(menu);
+                start();
+            case "exit":
+                System.out.println("Bye!");
+                return;
+            default:
+                System.out.println("Wrong meal category! Choose from: breakfast, lunch, dinner.");
+                start();
+        }
+    }
+}

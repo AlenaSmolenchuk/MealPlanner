301ac43bfe14a3c79f4cce0a153aa961cea6ca6e
Alena <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-04-24 19:04:49 +0300

    Update Menu.java

--- src/mealplanner/Menu.java
+++ src/mealplanner/Menu.java
@@ -1,36 +1,25 @@
 package mealplanner;
 
-import java.util.HashMap;
 import java.util.Scanner;
 
 public class Menu {
 
-    private HashMap<String,Meal> menu = new HashMap<>(10000000);
-    Scanner sc = new Scanner(System.in);
+    private static final Scanner sc = new Scanner(System.in);
     Recept recept = new Recept();
 
     public void start(){
-        boolean check = true;
-        while(check) {
+        boolean exit = false;
+
+        while(!exit) {
             System.out.println("What would you like to do (add, show, exit)?");
-            String output = sc.nextLine();
-            switch (output) {
-                case "add":
-                    menu = recept.addRecept();
-                    break;
-                case "show":
-                    recept.printRecept(menu);
-                    break;
-                case "exit":
-                    System.out.println("Bye!");
-                    check = false;
-                    break;
-                default:
-                    break;
-            }
-            if(!check){
-                break;
+            switch (Command.getCommand(sc.nextLine())) {
+                case ADD -> recept.addRecept();
+                case SHOW -> recept.showRecept();
+                case EXIT -> exit = true;
+                case UNKNOWN -> {
+                }
             }
         }
+        System.out.println("Bye!");
     }
 }

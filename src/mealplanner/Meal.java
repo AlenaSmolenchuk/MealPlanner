b35a9d92fcf2c68c437d538f1aff758814af8c5f
Alena <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-04-24 19:03:13 +0300

    Update Meal.java

--- src/mealplanner/Meal.java
+++ src/mealplanner/Meal.java
@@ -1,34 +1,18 @@
 package mealplanner;
-    
-public class Meal {
-   
-    private String name;
-    private String[] ingredients;
- 
-    public Meal(String name, String[] ingredients) {
-        this.name = name;
-        this.ingredients = ingredients;
-    }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String[] getIngredients() {
-        return ingredients;
-    }
-
-    public void setIngredients(String[] ingredients) {
-        this.ingredients = ingredients;
-    }
+public record Meal(Category category, String name, String[] ingredients) {
 
     @Override
     public String toString() {
-        return "Name: " + name + "\n" +
-                "Ingredients: " + String.join("\n", ingredients);
+        return String.format("""
+                      
+                        Category: %s
+                        Name: %s
+                        Ingredients:
+                        %s""",
+                category.name,
+                name,
+                String.join("\n", ingredients)
+        );
     }
 }

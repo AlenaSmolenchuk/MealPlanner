c43b36c240f8800cda60af2066fbb5194d3cc4b3
Alena <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-04-28 20:47:11 +0300

    Update Category.java

--- src/mealplanner/Category.java
+++ src/mealplanner/Category.java
@@ -10,7 +10,7 @@ public enum Category {
     Category(String name) {
         this.name = name;
     }
- 
+
     public static Category getCategory(String input) {
         for (Category category : Category.values()) {
             if (category.name.equals(input)) return category;

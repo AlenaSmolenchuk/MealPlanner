9b506a0d5b2896bc282819713280c3152ebd537d
Alena <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-05-03 13:45:37 +0300

    Update Menu.java

--- src/mealplanner/Menu.java
+++ src/mealplanner/Menu.java
@@ -1,37 +1,63 @@
 package mealplanner;
 
+
+import java.sql.SQLException;
+
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Menu {
-    private static final Scanner sc = new Scanner(System.in);
-    Recept recept = new Recept();
+    boolean isOver = false;
 
-    public void start(){
-        Tables.init();
-        boolean exit = true;
-
-        while(exit) {
-            System.out.println("What would you like to do (add, show, exit)?");
-            String output = sc.nextLine();
-            switch (output) {
-                case "add" :
-                    recept.addRecept();
-                    break;
-                case "show" :
-                    recept.showRecept();
-                    break;
-                case "exit" :
-                    System.out.println("Bye!");
-                    exit = false;
-                    break;
-                default:
-                    break;
-            }
-        if (!exit){
-            break;
-        }
+    public void run(Database database) throws SQLException {
+        switch (makeChoice()) {
+            case "show":
+                database.showTables();
+            case "add" :
+                System.out.println("Which meal do you want to add (breakfast, lunch, dinner)?");
+                database.insertMeal(getCategoryInput(),getNameInput(),getIngredients());
+            case "exit" :
+            isOver = true;
         }
     }
+
+    public String makeChoice() {
+        System.out.println("What would you like to do (add, show, exit)?");
+        return new Scanner(System.in).nextLine().toLowerCase();
+    }
+
+    private String getCategoryInput() {
+        List<String> meals = List.of("breakfast", "lunch", "dinner");
+        String input = new Scanner(System.in).nextLine().toLowerCase();
+        if (!meals.contains(input)) {
+            System.out.println("Wrong meal category! Choose from: breakfast, lunch, dinner.");
+            return getCategoryInput();
+        }
+        return input;
+    }
+
+    public String getNameInput() {
+        System.out.println("Input the meal's name:");
+        String input = new Scanner(System.in).nextLine().toLowerCase();
+        if (Checking.wrongFormat(input) || input.isBlank() || input.isEmpty()) {
+            System.out.println("Wrong format. Use letters only!");
+            return getNameInput();
+        }
+        return input;
+    }
+
+    public List<String> getIngredients() {
+        System.out.println("Input the ingredients:");
+        List<String> input = List.of(new Scanner(System.in).nextLine().split(", "));
+        for (String ingredients : input) {
+            if (Checking.invalidIngredient(ingredients) || ingredients.isBlank()) {
+                System.out.println("Wrong format. Use letters only!");
+                return getIngredients();
+            }
+        }
+        return input;
+    }
 }
- 
+
 

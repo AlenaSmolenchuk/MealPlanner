4ac769707ab78eba6cbbd8032c61f97de996b6b7
Alena <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-06-15 13:40:57 +0300

    Update Menu.java

--- src/mealplanner/Menu.java
+++ src/mealplanner/Menu.java
@@ -7,9 +7,11 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Menu {
+   
     boolean isOver = false;
 
     public void run(Database database) throws SQLException {
+       
         switch (makeChoice()) {
             case "show":
                 database.showTables();
@@ -22,13 +24,18 @@ public class Menu {
     }
 
     public String makeChoice() {
+        
         System.out.println("What would you like to do (add, show, exit)?");
+        
         return new Scanner(System.in).nextLine().toLowerCase();
     }
 
     private String getCategoryInput() {
+        
         List<String> meals = List.of("breakfast", "lunch", "dinner");
+        
         String input = new Scanner(System.in).nextLine().toLowerCase();
+        
         if (!meals.contains(input)) {
             System.out.println("Wrong meal category! Choose from: breakfast, lunch, dinner.");
             return getCategoryInput();
@@ -37,8 +44,11 @@ public class Menu {
     }
 
     public String getNameInput() {
+       
         System.out.println("Input the meal's name:");
+        
         String input = new Scanner(System.in).nextLine().toLowerCase();
+        
         if (Checking.wrongFormat(input) || input.isBlank() || input.isEmpty()) {
             System.out.println("Wrong format. Use letters only!");
             return getNameInput();
@@ -47,14 +57,17 @@ public class Menu {
     }
 
     public List<String> getIngredients() {
+        
         System.out.println("Input the ingredients:");
+        
         List<String> input = List.of(new Scanner(System.in).nextLine().split(", "));
+        
         for (String ingredients : input) {
             if (Checking.invalidIngredient(ingredients) || ingredients.isBlank()) {
                 System.out.println("Wrong format. Use letters only!");
                 return getIngredients();
             }
         }
-        return input;
+       return input;
     }
 }

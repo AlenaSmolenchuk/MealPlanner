6dd6d419b346cb6f25b8f75361cd4c8d4d69e5cf
Alena <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-06-15 13:37:46 +0300

    Update Checking.java

--- src/mealplanner/Checking.java
+++ src/mealplanner/Checking.java
@@ -5,11 +5,14 @@ import java.util.Objects;
 public class Checking {
 
     protected static boolean wrongFormat(String name){
+        
         if(name.isBlank()) return true;
+        
         return !name.matches("[a-zA-Z ]+");
     }
 
     protected static boolean invalidIngredient(String ingredient){
+       
         return !ingredient.matches("[a-zA-Z ]+");
     }
 }
